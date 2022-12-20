82819217e302091070b52dec3f24bc2ed7c6e22d
shivanurag <77777785+shivanurag@users.noreply.github.com>
2022-12-20 18:41:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function rc()
+{
+    location.href="https://en.wikipedia.org/wiki/Rio_Carnival";
+}
+function c()
+{
+    location.href="https://en.wikipedia.org/wiki/Christmas";
+}
+function d()
+{
+    location.href="https://en.wikipedia.org/wiki/Diwali";
+}
+function cny()
+{
+    location.href="https://en.wikipedia.org/wiki/Chinese_New_Year";
+}
+function dotd()
+{
+    location.href="https://en.wikipedia.org/wiki/Day_of_the_Dead";
+}
+function e()
+{
+    location.href="https://en.wikipedia.org/wiki/Easter";
+}
